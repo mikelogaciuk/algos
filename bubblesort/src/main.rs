7b305e7f11103df22168f903a64ee9ac8ac2d15b
Mike Logaciuk <55132446+mikelogaciuk@users.noreply.github.com>
2024-02-01 16:53:31 +0100

    Add bubblehsort

--- bubblesort/src/main.rs
+++ bubblesort/src/main.rs
@@ -1,8 +1,11 @@
 use rand::Rng;
 
-
 fn main() {
-    println!("Hello, world!");
+    let swamp = make_unsorted_arr(16);
+
+    let sorted: Vec<i32> = bubble_sort(swamp);
+
+    println!("Sorted array: {:?}", sorted);
 }
 
 fn make_unsorted_arr(n: i16) -> Vec<i32> {
@@ -12,12 +15,37 @@ fn make_unsorted_arr(n: i16) -> Vec<i32> {
     return arr;
 }
 
-fn xd(unsorted_arr: Vec<i32>) -> Vec<i32> {
-    let n: usize = unsorted_arr.len();
+fn bubble_sort(arr: Vec<i32>) -> Vec<i32> {
+    let mut sorted_arr = arr.clone();
+    let mut swapped = true;
 
-    loop {
-        let mu
+    while swapped {
+        swapped = false;
+        for i in 0..sorted_arr.len() - 1 {
+            if sorted_arr[i] > sorted_arr[i + 1] {
+                sorted_arr.swap(i, i + 1);
+                swapped = true;
+            }
+        }
     }
 
+    return sorted_arr;
+}
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_make_unsorted_arr() {
+        let arr = make_unsorted_arr(16);
+        assert_eq!(arr.len(), 16);
+    }
+
+    #[test]
+    fn test_bubble_sort() {
+        let arr = vec![6, 8, 14, 5, 1, 2, 4, 99, 16, 6];
+        let sorted_arr = bubble_sort(arr);
+        assert_eq!(sorted_arr, vec![1, 2, 4, 5, 6, 6, 8, 14, 16, 99]);
+    }
 }
